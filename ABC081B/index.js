2a9a0761034c09be30423f58c26cdc547684e294
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2019-01-05 14:36:45 +0900

    ABC081B

--- ABC081B/index.js
+++ ABC081B/index.js
@@ -2,11 +2,28 @@
 function Main(input) {
   input = input.split("\n");
   const count = input[0];
-  const arr = input[1].split("");
+  const arr = input[1].split(" ").slice(0, count);
+  console.log(countSplit(arr, 0));
 }
 
-function divAll(arr) {
+const divAll = arr => {
   return arr.map(e => e / 2);
-}
+};
+
+const isExistOdd = arr => {
+  return arr.some(e => e % 2 !== 0);
+};
+
+const isNotAllZero = arr => {
+  return arr.find(e => e !== 0);
+};
+
+const countSplit = (arr, count) => {
+  if (isExistOdd(arr) || !isNotAllZero(arr)) {
+    return count;
+  }
+  count = count += 1;
+  return countSplit(divAll(arr), count);
+};
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
