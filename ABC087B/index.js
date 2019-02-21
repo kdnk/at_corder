92721358e7fdb7be244059530fe85597c25eb9ad
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2019-02-21 23:25:02 +0900

    wip

---
+++ ABC087B/index.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+const VALUE = {
+  a: 500,
+  b: 100,
+  c: 50
+};
+
+function Main(input) {
+  input = input.split("\n");
+  const a = input[0];
+  const b = input[1];
+  const c = input[2];
+  const x = input[3];
+}
+
+const calcX = (a, b, c, x) => {};
+
+Main(require("fs").readFileSync("/dev/stdin", "utf8"));
