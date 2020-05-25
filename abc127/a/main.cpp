dc4eee9e5b532eb4e96b09ccf3b423b3be199e8b
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2020-05-25 22:19:57 +0900

    .

--- abc127/a/main.cpp
+++ abc127/a/main.cpp
@@ -1,5 +1,16 @@
+// N 枚の ID カードと
+// M 個のゲートがあります。
+
+// i 番目のゲートは
+// Li, Li+1, ..., Ri番目の ID カードのうちどれか
+// 1枚を持っていれば通過できます。
+
+// 1枚だけで全てのゲートを通過できる ID カードは何枚あるでしょうか。
+// 1 ≤ Li ≤ Ri ≤ N
+
 #include <bits/stdc++.h>
 using namespace std;
+typedef signed long long ll;
 
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
@@ -7,16 +18,12 @@ int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
 
-  int a, b;
-  cin >> a >> b;
+  int N, M;
+  cin >> N >> M;
+  vector<int> L(M);
+  vector<int> R(M);
 
-  if (13 <= a) {
-    cout << b << endl;
-  } else if (6 <= a && a < 13) {
-    cout << b / 2 << endl;
-  } else {
-    cout << 0 << endl;
-  }
+  rep(i, M) { cin >> L[i] >> R[i]; }
 
   return 0;
 }
